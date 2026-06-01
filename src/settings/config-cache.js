f27c349d6c3234f4c8053745e87acb617574d2b7
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-06-01 16:47:00 +0700

    Add ConfigCache class for caching configuration

---
+++ src/settings/config-cache.js
@@ -0,0 +1,23 @@
+export class ConfigCache {
+
+    #cache = new Map();
+
+    set(key, value) {
+
+        this.#cache.set(key, value);
+
+    }
+
+    get(key) {
+
+        return this.#cache.get(key);
+
+    }
+
+    has(key) {
+
+        return this.#cache.has(key);
+
+    }
+
+} 

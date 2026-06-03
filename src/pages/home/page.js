03ed395a7a735f2328ed842b6ee491c76bc14870
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-06-04 00:22:02 +0700

    Add HomePage class with render method

---
+++ src/pages/home/page.js
@@ -0,0 +1,22 @@
+export class HomePage {
+
+    render() {
+
+        const root =
+            document.createElement(
+                "section"
+            );
+
+        root.innerHTML = `
+
+            <h1>
+                Home
+            </h1>
+
+        `;
+
+        return root;
+
+    }
+
+} 

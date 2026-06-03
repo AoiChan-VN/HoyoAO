73fa850a8e865070706f0f9c9272beb3a518715b
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-06-04 00:24:20 +0700

    Add NotFoundPage class for 404 error handling

---
+++ src/pages/not-found/page.js
@@ -0,0 +1,22 @@
+export class NotFoundPage {
+
+    render() {
+
+        const root =
+            document.createElement(
+                "section"
+            );
+
+        root.innerHTML = `
+
+            <h1>
+                404
+            </h1>
+
+        `;
+
+        return root;
+
+    }
+
+} 

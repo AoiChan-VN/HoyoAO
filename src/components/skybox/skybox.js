e6e474602df8d107ac9ec70558fa6b986df5a407
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-06-05 01:46:46 +0700

    Implement texture application for skybox
    Added a method to apply textures based on the environment.

--- src/components/skybox/skybox.js
+++ src/components/skybox/skybox.js
@@ -6,6 +6,39 @@ export class VRStatusSkybox extends HTMLElement {
 
     connectedCallback() {
         this.render();
+        this._applyTextures();
+    }
+
+    _applyTextures() {
+        const isGitHubPages = window.location.hostname.includes('github.io');
+        let basePath = '';
+        
+        if (isGitHubPages) {
+            const pathSegments = window.location.pathname.split('/');
+            const repoName = pathSegments[1];
+            basePath = `/${repoName}/`;
+        } else {
+            basePath = '/';
+        }
+
+        const cleanBasePath = basePath.endsWith('/') ? basePath : basePath + '/';
+        const texturesPath = `${window.location.origin}${cleanBasePath}src/assets/textures/`;
+
+        const faces = {
+            'front': 'pz.webp',
+            'back': 'nz.webp',
+            'left': 'px.webp',
+            'right': 'nx.webp',
+            'top': 'py.webp',
+            'bottom': 'ny.webp'
+        };
+
+        for (const [faceClass, fileName] of Object.entries(faces)) {
+            const element = this.shadowRoot.querySelector(`.${faceClass}`);
+            if (element) {
+                element.style.backgroundImage = `url('${texturesPath}${fileName}')`;
+            }
+        }
     }
 
     render() {

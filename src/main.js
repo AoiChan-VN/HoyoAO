9171aef67f3a3b3807a6ce4c6950a2fcfbd304e6
༺◡🥀𝓐𝓸𝓲𝓒𝓱𝓪𝓷🥀◡༻ <121602736+AoiChan-VN@users.noreply.github.com>
2026-06-06 20:15:59 +0700

    Refactor imports in main.js for cleaner structure

--- src/main.js
+++ src/main.js
@@ -1,29 +1,12 @@
-import SpatialRegistry
-from "./core/SpatialRegistry.js";
-
-import Camera
-from "./core/Camera.js";
-
-import MatrixRenderer
-from "./core/MatrixRenderer.js";
-
-import PhysicsEngine
-from "./core/PhysicsEngine.js";
-
-import Engine
-from "./core/Engine.js";
-
-import InputSystem
-from "./systems/InputSystem.js";
-
-import TouchSystem
-from "./systems/TouchSystem.js";
-
-import DragSystem
-from "./systems/DragSystem.js";
-
-import SpatialPhysicsSystem
-from "./systems/SpatialPhysicsSystem.js";
+import SpatialRegistry from "./core/SpatialRegistry.js";
+import Camera from "./core/Camera.js";
+import MatrixRenderer from "./core/MatrixRenderer.js";
+import PhysicsEngine from "./core/PhysicsEngine.js";
+import Engine from "./core/Engine.js";
+import InputSystem from "./systems/InputSystem.js";
+import TouchSystem from "./systems/TouchSystem.js";
+import DragSystem from "./systems/DragSystem.js";
+import SpatialPhysicsSystem from "./systems/SpatialPhysicsSystem.js";
 
 import "./components/spatial-menu/SpatialMenu.js";
 import "./components/profile-panel/ProfilePanel.js";
